49d7987d5c15f0be8a24a50e99302f922ca69fee
Tatul Minasyan <a1234@MacBook-Air-15.local>
2025-12-12 12:40:52 +0400

    server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export',
+    basePath: isProd ? '/Portfolio-Website' : '',
+    assetPrefix: isProd ? '/Portfolio-Website/' : '',
+};
 
 export default nextConfig;
